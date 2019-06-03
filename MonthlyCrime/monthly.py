dc2318cb39272aab883ff00631ce8f42403e8202
Paul Rodriguez <prodr010@ucr.edu>
2019-06-03 15:19:30 -0700

    new monthly graphs

--- MonthlyCrime/monthly.py
+++ MonthlyCrime/monthly.py
@@ -2,7 +2,6 @@ import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 import scipy.linalg as la
-
 '''
 print("loading time series1... ")
 timeSeries = np.loadtxt('TheftYearMonth.txt',delimiter=',',dtype=float) # load data
@@ -18,6 +17,7 @@ print("loading time series3... ")
 timeSeries = np.loadtxt('CriminalDamageYearMonth.txt',delimiter=',',dtype=float) # load data
 monthly = timeSeries[:,0]
 plt.plot(monthly,'b-', label='CriminalDamage')
+
 print("loading time series4... ")
 timeSeries = np.loadtxt('NarcoticsYearMonth.txt',delimiter=',',dtype=float) # load data
 monthly = timeSeries[:,0]
@@ -26,13 +26,15 @@ plt.plot(monthly,'c-', label='Narcotics')
 print("loading time series5... ")
 timeSeries = np.loadtxt('AssaultYearMonth.txt',delimiter=',',dtype=float) # load data
 monthly = timeSeries[:,0]
-labels = timeSeries[:,1]
 plt.plot(monthly,'m-', label='Assault')
 
+l = ['2001','2002','2003','2004','2005','2006','2007','2008','2009','2010','2011'
+,'2012','2013','2014','2015','2016','2017','2018','2019']
+
 print("plotting...")
-plt.xticks(np.arange(1,(12*18)+1,step=12))
+plt.xticks(np.arange(1,(12*18)+2,step=12),l)
 plt.grid(True)
 plt.legend()
-plt.axis([1,216,0,10100])
-plt.title('Crime per Month')
+plt.axis([1,217,0,10100])
+plt.title('Crime per Year')
 plt.show()

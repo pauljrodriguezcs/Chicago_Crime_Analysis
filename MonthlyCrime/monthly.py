44b4992f0a60ea2a28bc28b7b749888b1dcef324
pauljrodriguezcs <prodr010@ucr.edu>
2019-06-03 12:14:15 -0700

    MonthlyCrimeDone

--- MonthlyCrime/monthly.py
+++ MonthlyCrime/monthly.py
@@ -3,36 +3,36 @@ import matplotlib
 import matplotlib.pyplot as plt
 import scipy.linalg as la
 
+'''
 print("loading time series1... ")
+timeSeries = np.loadtxt('TheftYearMonth.txt',delimiter=',',dtype=float) # load data
+monthly = timeSeries[:,0]
+plt.plot(monthly,'r-', label='Theft')
+
+print("loading time series2... ")
+timeSeries = np.loadtxt('BatteryYearMonth.txt',delimiter=',',dtype=float) # load data
+monthly = timeSeries[:,0]
+plt.plot(monthly,'g-', label='Battery')
+
+print("loading time series3... ")
+timeSeries = np.loadtxt('CriminalDamageYearMonth.txt',delimiter=',',dtype=float) # load data
+monthly = timeSeries[:,0]
+plt.plot(monthly,'b-', label='CriminalDamage')
+print("loading time series4... ")
+timeSeries = np.loadtxt('NarcoticsYearMonth.txt',delimiter=',',dtype=float) # load data
+monthly = timeSeries[:,0]
+plt.plot(monthly,'c-', label='Narcotics')
+'''
+print("loading time series5... ")
 timeSeries = np.loadtxt('AssaultYearMonth.txt',delimiter=',',dtype=float) # load data
 monthly = timeSeries[:,0]
 labels = timeSeries[:,1]
-plt.plot(monthly,'r-', label='assault')
-
-# print("loading time series2... ")
-# timeSeries = np.loadtxt('BatteryTS.txt',delimiter=',',dtype=float) # load data
-# hourly = timeSeries[:,0]
-# plt.plot(hourly,'b-', label='battery')
-
-# print("loading time series3... ")
-# timeSeries = np.loadtxt('CriminalDamageTS.txt',delimiter=',',dtype=float) # load data
-# hourly = timeSeries[:,0]
-# plt.plot(hourly,'m-', label='CriminalDamage')
-
-# print("loading time series4... ")
-# timeSeries = np.loadtxt('TarcoticsTS.txt',delimiter=',',dtype=float) # load data
-# hourly = timeSeries[:,0]
-# plt.plot(hourly,'c-', label='narcotics')
-
-# print("loading time series5... ")
-# timeSeries = np.loadtxt('TheftTS.txt',delimiter=',',dtype=float) # load data
-# hourly = timeSeries[:,0]
-# plt.plot(hourly,'k-', label='theft')
+plt.plot(monthly,'m-', label='Assault')
 
 print("plotting...")
-l = np.arange(2001,2018,step=12)
-plt.xticks(np.arange(216),l)
+plt.xticks(np.arange(1,(12*18)+1,step=12))
 plt.grid(True)
 plt.legend()
+plt.axis([1,216,0,10100])
 plt.title('Crime per Month')
 plt.show()

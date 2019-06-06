5e4916b65f41b3e12bb7d0effd0282064be253f7
Paul Rodriguez <prodr010@ucr.edu>
2019-06-05 19:25:39 -0700

    higher res graphs created

--- HourlyCrime/hour.py
+++ HourlyCrime/hour.py
@@ -4,30 +4,33 @@ import matplotlib.pyplot as plt
 import scipy.linalg as la
 
 print("loading time series... ")
-timeSeries = np.loadtxt('AssaultTS.txt',delimiter=',',dtype=float) # load data
-print("done ... ")
+plt.figure(figsize=(16,9))
 
+timeSeries = np.loadtxt('TheftTS.txt',delimiter=',',dtype=float) # load data
 hourly = timeSeries[:,0]
-plt.plot(hourly,'r-', label='assault')
+plt.plot(hourly,'r-', label='theft')
 
 timeSeries = np.loadtxt('BatteryTS.txt',delimiter=',',dtype=float) # load data
 hourly = timeSeries[:,0]
-plt.plot(hourly,'b-', label='battery')
+plt.plot(hourly,'g-', label='battery')
 
 timeSeries = np.loadtxt('CriminalDamageTS.txt',delimiter=',',dtype=float) # load data
 hourly = timeSeries[:,0]
-plt.plot(hourly,'m-', label='CriminalDamage')
+plt.plot(hourly,'b-', label='CriminalDamage')
 
 timeSeries = np.loadtxt('TarcoticsTS.txt',delimiter=',',dtype=float) # load data
 hourly = timeSeries[:,0]
 plt.plot(hourly,'c-', label='narcotics')
 
-timeSeries = np.loadtxt('TheftTS.txt',delimiter=',',dtype=float) # load data
+timeSeries = np.loadtxt('AssaultTS.txt',delimiter=',',dtype=float) # load data
 hourly = timeSeries[:,0]
-plt.plot(hourly,'k-', label='theft')
+plt.plot(hourly,'m-', label='assault')
 
 plt.xticks(np.arange(0,24,step=1))
 plt.grid(True)
 plt.legend()
+plt.xlabel('Hour')
+plt.ylabel('Total Crimes')
 plt.title('Crime per Hour')
-plt.show()
+# plt.show()
+plt.savefig('CrimePerHour.png',format='png',dpi=600)
